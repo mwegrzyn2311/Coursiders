2b059c0ea1dff19a48816fc4c8cf851ec160b437
Michal23 <michal.wegrzyn2137@gmail.com>
2020-02-08 15:04:17 +0100

    fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBDp01wCjbHZMh3t6wWrkv4nU_SDQ42lvU',
+    authDomain: 'coursiders.firebaseapp.com',
+    databaseURL: 'https://coursiders.firebaseio.com',
+    projectId: 'coursiders',
+    storageBucket: 'coursiders.appspot.com',
+    messagingSenderId: '932740632594',
+  }
 };
